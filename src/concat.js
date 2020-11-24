4de64a337d5f50ff226a06e507f2655fef89ef1a
PalomaLP <palomaledesmap@gmail.com>
2020-11-24 09:31:34 +0100

    First version

---
+++ src/concat.js
@@ -0,0 +1,16 @@
+//2. CONCAT.
+
+//Implementea una función concat, tal que, dados 2 arrays como entrada, devuelva la concatenación de ambos.Utilizar rest y spread operators.
+
+const animals = ["snail", "turtle", "camel"];
+const colours = ["brown", "gree", "pink"];
+
+
+const ejemplo = [...animals];
+
+// const concat = (array1, array2) => {
+//     return [...array1] + [...array2]
+// }
+
+console.log ();
+
